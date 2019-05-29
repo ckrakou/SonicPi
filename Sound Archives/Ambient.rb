c6fe8aa29fb74eb1fbee5ae6d67552707e9efdbf
Christoffer - from home <chris.krakou@gmail.com>
2019-05-29 20:48:53 +0200

    Created an organ, adjusted amplitudes

--- Sound Archives/Ambient.rb
+++ Sound Archives/Ambient.rb
@@ -2,20 +2,32 @@
 live_loop :Heartbeat do
   with_fx :gverb, mix: 0.5,damp: 0.9 do
     with_fx :echo do
-      #sample :bd_tek, rate: 0.5, lpf: 80
+      sample :bd_tek, rate: 0.5, lpf: 80
       sleep 2
     end
   end
 end
 
-# better choir. Adjust transpose up for angelic
-live_loop :twinkle do
-  with_fx :whammy, transpose: 0,grainsize: 0.25 do
-    with_fx :vowel,voice: 0, vowel_sound: range(1,5,1).choose do
-      sample :ambi_choir, amp: 2, beat_stretch: 8
+# better choir. Adjust transpose up for angelic. really quiet
+live_loop :choir do
+  with_fx :whammy,amp: 5, transpose: 12,grainsize: 0.25 do
+    with_fx :vowel,pre_amp: 5,voice: 0, vowel_sound: range(1,5,1).choose do
+      sample :ambi_choir, amp: 1, beat_stretch: 8
       sleep sample_duration(:ambi_choir) * 4
     end
   end
 end
 
+# Organ. Flanger gives it more body. Season with resonance
+# Works well with beep, tb303, supersaw, techsaw, fm
+live_loop :organ do
+  with_fx :gverb, mix: 0.25, amp: 0.5 do
+    with_fx :flanger do
+      with_fx :octaver do
+        synth :fm, note: (chord :c4, :augmented).choose, sustain: 8, attack: 1, release: 1, amp: 1
+        sleep 9
+      end
+    end
+  end
+end
 
