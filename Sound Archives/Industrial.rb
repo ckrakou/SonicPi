bb06568cd802d4168b32dc8f0955ecce0de9ea34
Christoffer - from home <chris.krakou@gmail.com>
2019-05-30 22:14:42 +0200

    Update Industrial.rb

--- Sound Archives/Industrial.rb
+++ Sound Archives/Industrial.rb
@@ -1,9 +1,9 @@
 # Thundering Drums
 live_loop :drums do
-  with_fx :distortion, distort: 0.7 do
+  with_fx :distortion, distort: 0.5 do
     with_fx :reverb, damp: 1 do
       64.times do
-        sample :bd_tek, rate: 0.5, amp: 1 if (spread 3,12).tick
+        sample :bd_tek, rate: 0.5 ,amp: 1 if (spread 9,12).tick
         sample :elec_snare, rate: 0.5, amp: 1 if (spread 7,12).look
         sleep 0.125
       end
