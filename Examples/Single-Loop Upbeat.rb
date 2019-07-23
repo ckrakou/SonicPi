29ee74793b4be9f4284820004ade52de696cf029
Christoffer - Stationær <chris.krakou@gmail.com>
2019-07-23 14:30:57 +0200

    Changes based on feedback

--- Examples/Single-Loop Upbeat.rb
+++ Examples/Single-Loop Upbeat.rb
@@ -1,18 +1,18 @@
 use_bpm 120
 
 # Core Structure. This is the basis for our loops
-# If you have more than one loop, make sure to synchronize them
+# To hear each loop, put a # in from of "stop" and run the code.
 live_loop :main do
   stop
   64.times do
     tick
     
-    
     sleep 0.25
   end
 end
 
-# simple rythm w. euclidian algorithms and rotations
+# Let's make some rhythms for our samples
+# We'll use the euclidean algorithm to quickly create some rhythms
 live_loop :main2 do
   stop
   64.times do
@@ -20,38 +20,53 @@ live_loop :main2 do
     
     # this is our bass drum rythm. we'll do 7 hits over 16 steps.
     # to say it in a way that is a bit easier to read, our rythm is: "*__*_*_*__*_*_*_"
-    kick_rythm = (ring true, false, false, true, false, true, false, true, false, false, true, false, true, false, true, false)
+    kick_rhythm = (ring true, false, false, true, false, true, false, true, false, false, true, false, true, false, true, false)
     
-    if kick_rythm.look == true then
-      sample :bd_tek if (spread 7, 16).look
+    if kick_rhythm.look == true then
+      sample :bd_tek
     end
     
+    # Let's save our snare pattern, which is 2 hits evenly spread over 16 steps
+    # giving us this: *_______*_______
+    snare_rhythm = (ring true,false,false,false,false,false,false,false,true,false,false,false,false,false,false,false)
     
-    # We can use the spread function to generate this list for us
-    # This gives us 2 hits spread evenly over 16 hits, and rotates the list 4 times
-    # giving us this: ____*_______*___
-    # Since it's short and concise, we'll put it straight in the "if" statement
-    if (spread 2,16).rotate(4).look then
+    
+    # We can use the spread function to generate this list for us instead of writing it out
+    # This gives us 2 hits spread evenly over 16 steps.
+    snare_rhythm = (spread 2,16)
+    
+    # because it's so much shorter, we can put it directly in the "if" statement.
+    if (spread 2,16).look then
       sample :elec_snare
     end
     
-    # Adding the if-statement to the end does the same thing as the normal "if" structure
-    # We'll use the short form for the rest of the example. It's faster to write out.
+    # We can also shift our rhytms by adding .rotate() at the end of the spread function
+    # imagine it as taking the last element and putting it in the front of the list
+    # the number in the parenthesis is how many times it does this.
+    # This one gives us the following pattern: *_*_*_**_*_*_**_
+    cymbal_rhythm = (spread 9,16).rotate(2)
+    
+    # Adding the if-statement to the end works the same as the normal "if" structure
+    # We'll use this short form for the rest of the examples. It's faster to write out.
+    
+    # If we put it all together, we can write our cymbal rhythms as just one line of code
     sample :drum_cymbal_closed if (spread 9,16).rotate(2).look
     sleep 0.25
   end
 end
 
-# With generative rythms. Messing with iterations for extra goodness
-# Keep the snare stable, otherwise it gets confusing.
+# Let's get crazy and generate some random rhythms. We'll use .choose instead of .look for this.
+# We'll save the number of hits in a separate list, and pick one for each iteration
+# We'll keep the snare at 2 hits per 16 steps, rotated by 4. Otherwise it gets too chaotic
 live_loop :main3 do
   stop
-  kicks = (ring 5,7,9).choose # get our kicks
-  cymbals = (ring 9,11,13).choose # Generate cymbal rythms
+  kicks = (ring 5,7,9).choose # These are our kicks
+  cymbals = (ring 9,11,13).choose # These are our cymbals
   
   # let's vary the number of iterations, to make it extra snappy
   (ring 16,32,64).choose.times do
     tick
+    # Notice how we're using the variable in place of the numbers
     sample :bd_tek if (spread kicks,16).look
     sample :elec_snare if (spread 2,16).rotate(4).look
     sample :drum_cymbal_closed if (spread cymbals,16).rotate(2).look
@@ -63,15 +78,20 @@ end
 # basis of our soundscape.
 live_loop :main4 do
   stop
-  kicks = (ring 5,7,9).choose # get our kicks
-  cymbals = (ring 9,11,13).choose # Generate cymbal rythms
-  bass_pitch = (scale :c0, :minor).choose - 12   # let's change up the pitch of the bass.
+  kicks = (ring 5,7,9).choose # These are our kicks
+  cymbals = (ring 9,11,13).choose # These are our cymbals
+  
+  # We'll use a scale to generate our bass pitches
+  # We could use a chord if we wanted to. Just make sure to start at 0
+  bass_pitch = (scale 0, :minor).choose
   
   (ring 16,32,64).choose.times do
     tick
     sample :bd_tek if (spread kicks,16).look
     sample :elec_snare if (spread 2,16).rotate(4).look
     sample :drum_cymbal_closed if (spread cymbals,16).rotate(2).look
+    
+    # we'll use the rpitch function to change the pitch of the bass sample.
     sample :bass_trance_c, rpitch: bass_pitch if (spread 1,16).look
     sleep 0.25
   end
@@ -84,9 +104,9 @@ end
 # We'll also adjust the amplitude of each sample.
 live_loop :main5 do
   stop
-  kicks = (ring 5,7,9).choose # get our kicks
-  cymbals = (ring 9,11,13).choose # Generate cymbal rythms
-  bass_pitch = (scale :c0, :minor).choose - 12   # Adjusted to account for the pitch of the bass sample
+  kicks = (ring 5,7,9).choose # These are our kicks
+  cymbals = (ring 9,11,13).choose # These are our cymbals
+  bass_pitch = (scale 0, :minor).choose # Our bass pitch
   
   (ring 16,32,64).choose.times do
     tick
@@ -108,19 +128,22 @@ end
 # so make sure you keep the notes high. We want them to stand out.
 live_loop :main6 do
   stop
-  kicks = (ring 5,7,9).choose # get our kicks
-  cymbals = (ring 9,11,13).choose # Generate cymbal rythms
-  bass_pitch = (scale :c0, :minor).choose - 12   # Adjusted to account for the pitch of the bass sample
-  lead_root = (scale :c4, :minor_pentatonic) # keep the lead interesting, since we tick tough it
+  kicks = (ring 5,7,9).choose # These are our kicks
+  cymbals = (ring 9,11,13).choose # These are our cymbals
+  bass_pitch = (scale 0, :minor).choose # Our bass pitch
+  
+  # This is the note for our lead. We'll generate these as well, to keep it more interesting
+  lead_note = (scale :c4, :minor_pentatonic).choose
   
   (ring 16,32,64).choose.times do
     tick
+    
     # Poor mans LFO.
     cut = line(70,110,steps: 32, inclusive: true).mirror.look
     
     # Our lead plays the same note, and the changing cutoff keeps it
-    # nice and interesting. You could iterate through chords instead
-    synth :supersaw, amp: 0.8, note: lead_root.look, cutoff: cut
+    # nice and interesting. You could iterate a chord instead
+    synth :supersaw, amp: 0.8, note: lead_note, cutoff: cut
     
     
     sample :bd_tek if (spread kicks,16).look
@@ -132,24 +155,23 @@ live_loop :main6 do
 end
 
 # Finally, let's add some nice effects.
-# Adding effects like this is really taxing on your computer
+# Adding effects like this can be really taxing on your computer
 # so be careful. You might want to separate your sounds into several loops.
 live_loop :final do
   stop
-  kicks = (ring 5,7,9).choose # get our kicks
-  cymbals = (ring 9,11,13).choose # Generate cymbal rythms
-  bass_pitch = (scale :c0, :minor).choose - 12   # Adjusted to account for the pitch of the bass sample
-  lead_root = (scale :c4, :minor_pentatonic).choose # keep the lead interesting, since we tick tough it
+  kicks = (ring 5,7,9).choose # These are our kicks
+  cymbals = (ring 9,11,13).choose # These are our cymbals
+  bass_pitch = (scale 0, :minor).choose # Our bass pitch
+  lead_notes = (scale :c4, :minor_pentatonic).choose # Our lead notes
   
   (ring 16,32,64).choose.times do
     tick
     # Poor mans LFO.
     cut = line(70,110,steps: 32, inclusive: true).mirror.look
-    lead_notes = (chord lead_root, :minor7) #  Let's keep these here. Easier to read the code
     
-    # Soften up the sound
-    with_fx :bpf, centre: lead_notes[2] do
-      synth :supersaw, amp: 0.8, note: lead_notes.look, cutoff: cut
+    # Soften up the sound a bit with a band pass filter
+    with_fx :bpf, centre: lead_notes, amp: 1.2 do
+      synth :supersaw, amp: 0.8, note: lead_notes, cutoff: cut
     end
     
     # Add a bit of punch to the drums
